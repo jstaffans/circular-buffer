41200ac3809fd698ec177a422bbb0f40d94a298c
jstaffans <johannes.staffans@gmail.com>
2013-03-11 23:16:29 +0200

    User interface

--- java/src/fi/bitrite/challenge/Main.java
+++ java/src/fi/bitrite/challenge/Main.java
@@ -1,9 +1,64 @@
 package fi.bitrite.challenge;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private static CircularBuffer buffer;
+
+    private static int numToAdd;
+    private static int numAdded;
+
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        CommandParser parser = new CommandParser();
+        while (true) {
+            System.out.print("circular-buffer> ");
+            String input = scanner.nextLine().trim();
+            parser.parse(input);
+        }
+    }
+
+    private static class CommandParser {
+
+        public void parse(String input) {
+            Scanner scanner = new Scanner(input);
+            String cmd = scanner.next();
+
+            if (numAdded < numToAdd) {
+                buffer.add(cmd);
+                numAdded++;
+                return;
+            }
+
+            if (cmd.equals("N")) {
+                if (scanner.hasNextInt()) {
+                    buffer = new CircularBuffer(scanner.nextInt());
+                }
+            }
+
+            if (cmd.equals("A")) {
+                if (scanner.hasNextInt()) {
+                    numToAdd = scanner.nextInt();
+                    numAdded = 0;
+                }
+            }
+
+            if (cmd.equals("R")) {
+                if (scanner.hasNextInt()) {
+                    buffer.remove(scanner.nextInt());
+                }
+            }
+
+            if (cmd.equals("L")) {
+                System.out.println(buffer.getItems());
+            }
+
+            if (cmd.equals("Q")) {
+                System.exit(0);
+            }
+
+        }
     }
 }
 
